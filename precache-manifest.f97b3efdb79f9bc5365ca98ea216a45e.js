9fb8a01aeaf0be92961ccab71c6ac6fd2dd44882
Alberto Zaranza <albertozaranza@outlook.com>
2019-12-20 02:42:32 -0300

    Updates

---
+++ precache-manifest.f97b3efdb79f9bc5365ca98ea216a45e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bb47132aba772549173",
+    "url": "/react-router-github-pages-example/static/css/main.54dd05a8.chunk.css"
+  },
+  {
+    "revision": "3bb47132aba772549173",
+    "url": "/react-router-github-pages-example/static/js/main.3bb47132.chunk.js"
+  },
+  {
+    "revision": "f12297194ac437e5d882",
+    "url": "/react-router-github-pages-example/static/js/runtime~main.f1229719.js"
+  },
+  {
+    "revision": "74777c5b45d9aa4c2a4d",
+    "url": "/react-router-github-pages-example/static/js/2.74777c5b.chunk.js"
+  },
+  {
+    "revision": "621b7c06d9269a09b6ea3f9e451dd696",
+    "url": "/react-router-github-pages-example/index.html"
+  }
+];
